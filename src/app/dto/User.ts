0830bee78b82f92777c5f0a3fce0515a7b119414
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-06-29 19:04:39 +0300

    Add more user info

--- src/app/dto/User.ts
+++ src/app/dto/User.ts
@@ -3,7 +3,10 @@ export class User {
   constructor(
     public login: string,
     public firstName: string,
+    public secondName: string,
     public lastName: string,
+    public email: string,
+    public phone: string,
     public role: string) {
   }
 }
