4e3441899700a6878993da8653383457b508bb1f
Jeka <uhp.gonchar.evgeniy@gmail.com>
2020-07-11 01:57:41 +0300

    Add request to rest

--- src/app/add-manager/add-manager.component.ts
+++ src/app/add-manager/add-manager.component.ts
@@ -1,22 +1,38 @@
 import {Component, OnInit} from '@angular/core';
 import {ManagerForm} from "../dto/ManagerForm";
+import {AdminService} from "../service/admin/adminService";
+import {HttpResponse} from "@angular/common/http";
+import {LoginService} from "../service/loginService";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-manager',
   templateUrl: './add-manager.component.html',
-  styleUrls: ['./add-manager.component.css']
+  styleUrls: ['./add-manager.component.css'],
+  providers: [AdminService, LoginService]
 })
 export class AddManagerComponent implements OnInit {
 
   addManagerForm = new ManagerForm("", "", "", "", "", "", "", "", "");
 
-  constructor() {
+  constructor(private adminService: AdminService,
+              private loginService: LoginService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    console.log(this.addManagerForm);
+    this.adminService.addManager(this.addManagerForm).subscribe(
+      (data: HttpResponse<any>) => {
+        console.log(data);
+        this.loginService.setToken(data.headers.get("Authorization").substring(7));
+        this.router.navigateByUrl('/home');
+      },
+      (error) => {
+          console.log(error);
+      }
+    );
   }
 }
--- src/app/service/admin/adminService.ts
+++ src/app/service/admin/adminService.ts
@@ -1,17 +1,29 @@
 import {Injectable} from "@angular/core";
 import {User} from "../../dto/User";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {ManagerForm} from "../../dto/ManagerForm";
 
 @Injectable({providedIn: 'root'})
 export class AdminService {
 
-  private url = 'http://localhost:8080/rest/admin/get-all-users';
+  private mainUrl = 'http://localhost:8080/rest/admin';
+
+  private getAllUsersUrl = this.mainUrl + '/get-all-users';
+
+  private addNewManagerUrl = this.mainUrl + '/new-manager';
+
+  private contentHeader = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private httpClient: HttpClient) {
   }
 
   getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(this.url);
+    return this.httpClient.get<User[]>(this.getAllUsersUrl);
+  }
+
+  addManager(managerForm: ManagerForm) {
+    return this.httpClient.post<any>(this.addNewManagerUrl,
+      managerForm, {headers: this.contentHeader, observe: 'response'});
   }
 }
